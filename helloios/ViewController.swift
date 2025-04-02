e476cc3b6de7017c78f73409b0bb42dc999319ad
TristanK117 <81729665+TristanK117@users.noreply.github.com>
2025-04-02 16:58:50 -0700

    Initial Commit

---
+++ helloios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by Tristan Khieu on 4/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ helloiosTests/helloiosTests.swift
@@ -0,0 +1,17 @@
+//
+//  helloiosTests.swift
+//  helloiosTests
+//
+//  Created by Tristan Khieu on 4/2/25.
+//
+
+import Testing
+@testable import helloios
+
+struct helloiosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
